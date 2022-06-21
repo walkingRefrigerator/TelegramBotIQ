7a82d7dd18766828711130e6f90ff2cbb995895e
Юрий Фролов <urafrolov0101@gmail.com>
2022-06-21 20:17:26 +0300

    refactoring.

--- TelegramBotIQ/BotCommands.cs
+++ TelegramBotIQ/BotCommands.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using MovieAPI;
+using System;
 using System.IO;
 using System.Threading.Tasks;
-
-using MovieAPI;
-
-using ConvertPDF;
-
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
-using System.Linq;
 
 namespace TelegramBotIQ
 {
@@ -39,17 +34,7 @@ namespace TelegramBotIQ
 
         #region Ответ на текст
 
-        public static async Task MessageMovie(Message msg, TelegramBotClient bot)
-        {
-            var text = msg?.Text;
-
-            if (text == null)
-                return;
-
-            await SendMovies(msg, bot);
-        }
-
-        public static async Task MessageReplyText(Message msg, TelegramBotClient bot, UserDB user)
+        public static async Task MessageReplyText(Message msg, TelegramBotClient bot)
         {
             var text = msg?.Text;
 
@@ -62,31 +47,12 @@ namespace TelegramBotIQ
 
                     await bot.SendTextMessageAsync(msg.Chat.Id, @"Список команд:
 /command - список команд,
-/Info - ссылки на разработчика,
 /Keyboard - запуск клавиатуры,
 /Menu - погода и курс валюты,
-/Movie - афиша фильмов,
-/MyFile - отправка ваших документов с облака,
-/Convert - конвертирование PDF в WORD,
-/Out - выход из пользователя.
+/Movie - афиша фильмов
 ");
                     break;
 
-
-                case "/Convert":
-
-                    var inlineFunc = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("PDF to Word")
-                        }
-                    }
-                    );
-
-                    await bot.SendTextMessageAsync(msg.Chat.Id, "Мои возможности", replyMarkup: inlineFunc);
-
-                    break;
 
                 case "/Movie":
 
@@ -107,97 +73,6 @@ namespace TelegramBotIQ
                     );
 
                     await bot.SendTextMessageAsync(msg.Chat.Id, "Мои возможности", replyMarkup: inlineF);
-
-                    break;
-
-                case "/Info":
-
-                    var inline = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithUrl("VK", "https://vk.com/y.frolovprogramfck"),
-                            InlineKeyboardButton.WithUrl("Telegram", "https://t.me/Yuryfralav")
-                        }
-                    }
-                        );
-
-                    await bot.SendTextMessageAsync(msg.Chat.Id, "Ссылки на моего разработчика", replyMarkup: inline);
-
-                    break;
-
-                case "/Keyboard":
-
-                    var keyboaerd1 = new ReplyKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            new KeyboardButton("Пришли фото"),
-                            new KeyboardButton("Мой контакт") {RequestContact = true}
-                        },
-                        new[]
-                        {
-                            new KeyboardButton("Что по деньгам?"),
-                            new KeyboardButton("Геолокация") {RequestLocation = true}
-                        }
-
-                    });
-
-                    await bot.SendTextMessageAsync(msg.Chat.Id, "Message", replyMarkup: keyboaerd1);
-
-                    break;
-
-                case "/MyFile":
-
-                    var keyboaerd2 = new ReplyKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            new KeyboardButton("Пришли мои документы"),
-                            new KeyboardButton("Пришли мои фото")
-                        },
-                        new[]
-                        {
-                            new KeyboardButton("Пришли мои голосовые"),
-                            new KeyboardButton("Пришли мои видео")
-                        },
-                        new[]
-                        {
-                            new KeyboardButton("Пришли мои аудио")
-                        }
-                    });
-
-                    await bot.SendTextMessageAsync(msg.Chat.Id, "Message", replyMarkup: keyboaerd2);
-
-                    break;
-
-                case "Пришли мои документы":
-
-                    await SendOfFileInfo(msg, bot, user, MessageType.Document);
-
-                    break;
-
-                case "Пришли мои фото":
-
-                    await SendOfPhoto(msg, bot, user, MessageType.Photo);
-
-                    break;
-
-                case "Пришли мои голосовые":
-
-                    await SendOfVoice(msg, bot, user, MessageType.Voice);
-
-                    break;
-
-                case "Пришли мои видео":
-
-                    await SendOfFileInfo(msg, bot, user, MessageType.Video);
-
-                    break;
-
-                case "Пришли мои аудио":
-
-                    await SendOfFileInfo(msg, bot, user, MessageType.Audio);
 
                     break;
 
@@ -243,207 +118,6 @@ namespace TelegramBotIQ
         }
         #endregion
 
-        #region Сохранение файлов типа Docs
-        public static async Task MessageReplyFile(Message msg, TelegramBotClient bot, MessageType type, UserDB user)
-        {
-            string file_id = "";
-            string directpath = "";
-            string path = "";
-
-            switch (type)
-            {
-                case MessageType.Audio:
-                    file_id = msg.Audio.FileId;
-
-                    directpath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-                    path = String.Format(@"TelegramBotIQ Users File\{0}\{1}\{2}.mp3", user.GetUser(), type.ToString(), msg.Audio.Title);
-
-                    break;
-
-                case MessageType.Photo:
-                    file_id = msg.Photo[0].FileId;
-
-                    directpath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-                    path = String.Format(@"TelegramBotIQ Users File\{0}\{1}\{2}.png", user.GetUser(), type.ToString(), msg.Photo[0]);
-
-                    break;
-
-                case MessageType.Voice:
-                    file_id = msg.Voice.FileId;
-
-                    directpath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-                    path = String.Format(@"TelegramBotIQ Users File\{0}\{1}\{2}", user.GetUser(), type.ToString(), msg.Voice.FileId);
-
-                    break;
-
-                case MessageType.Video:
-                    file_id = msg.Video.FileId;
-
-                    directpath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-                    path = String.Format(@"TelegramBotIQ Users File\{0}\{1}\{2}.mp4", user.GetUser(), type.ToString(), msg.Video.FileId);
-
-                    break;
-
-                case MessageType.Document:
-                    file_id = msg.Document.FileId;
-
-                    directpath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-                    path = String.Format(@"TelegramBotIQ Users File\{0}\{1}\{2}", user.GetUser(), type.ToString(), msg.Document.FileName);
-
-                    break;
-            }
-
-            
-
-            var dir = Directory.CreateDirectory(directpath);
-
-            try
-            {
-                using (var filestream = System.IO.File.OpenWrite(path))
-                {
-                    var filedowload = await bot.GetInfoAndDownloadFileAsync(
-                        fileId: file_id,
-                        destination: filestream
-                        );
-                }
-
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Ваш файл сохранен!");
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex}");
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Возникла ошибка :(" + Environment.NewLine +
-                    "Не беспокойтесь, разрабу дадим по шапке");
-            }
-        }
-        #endregion
-
-        public static async Task SendOfPhoto(Message msg, TelegramBotClient bot, UserDB user, MessageType type)
-        {
-            string filepath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-
-            try
-            {
-                DirectoryInfo directoryInfo = new DirectoryInfo(filepath);
-
-                foreach (var file in directoryInfo.GetFiles())
-                {
-                    FileStream file1 = System.IO.File.OpenRead(@$"{file.FullName}");
-
-                    InputOnlineFile inputOnline = new InputOnlineFile(file1, file1.Name);
-
-                    await bot.SendDocumentAsync(msg.Chat.Id, inputOnline);
-
-                }
-            }
-            catch
-            {
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Папка с файлами пуста");
-            }
-        }
-
-        public static async Task SendOfVoice(Message msg, TelegramBotClient bot, UserDB user, MessageType type)
-        {
-            string filepath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-
-            try
-            {
-                DirectoryInfo directoryInfo = new DirectoryInfo(filepath);
-
-                foreach (var file in directoryInfo.GetFiles())
-                {
-                    FileStream file1 = System.IO.File.OpenRead(@$"{file.FullName}");
-
-                    InputOnlineFile inputOnline = new InputOnlineFile(file1, file1.Name);
-
-                    await bot.SendVoiceAsync(msg.Chat.Id, inputOnline);
-
-                }
-            }
-            catch
-            {
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Папка с файлами пуста");
-            }
-        }
-
-        public static async Task SendOfFileInfo(Message msg, TelegramBotClient bot, UserDB user, MessageType type)
-        {
-            string filepath = String.Format(@"TelegramBotIQ Users File\{0}\{1}", user.GetUser(), type.ToString());
-
-            //string[] allFoundFiles = Directory.GetFiles("C:/Windows/", "gogo.txt", SearchOption.AllDirectories);
-
-            try
-            {
-                DirectoryInfo directoryInfo = new DirectoryInfo(filepath);
-
-                foreach (var file in directoryInfo.GetFiles())
-                {
-                    //FileStream file1 = System.IO.File.OpenRead(@$"{file.FullName}");
-
-                    //InputOnlineFile inputOnline = new InputOnlineFile(file1, file1.Name);
-
-                    //await bot.SendDocumentAsync(msg.Chat.Id, inputOnline);
-
-                    await bot.SendTextMessageAsync(msg.Chat.Id, file.Name);
-                }
-
-                var inlineKeyboard = new InlineKeyboardMarkup(new[]
-                {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("Скачать файл"),
-                            InlineKeyboardButton.WithCallbackData("Удалить файл")
-                        }
-                });
-
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Выберите действие", replyMarkup: inlineKeyboard);
-
-            }
-            catch
-            {
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Папка с файлами пуста");
-            }
-        }
-
-        public static async Task SendFile(Message msg, TelegramBotClient bot, UserDB user)
-        {
-            string[] allFoundFiles = Directory.GetFiles(@$"TelegramBotIQ Users File\{user.GetUser()}\", msg.Text, SearchOption.AllDirectories);
-
-            foreach (var filepath in allFoundFiles)
-            {
-                FileStream file = System.IO.File.OpenRead(filepath);
-
-                InputOnlineFile onlineFile = new InputOnlineFile(file, msg.Text);
-
-                if (filepath.Contains(MessageType.Document.ToString()))
-                    await bot.SendDocumentAsync(msg.Chat.Id, onlineFile);
-                else if (filepath.Contains(MessageType.Audio.ToString()))
-                    await bot.SendAudioAsync(msg.Chat.Id, onlineFile);
-                else if (filepath.Contains(MessageType.Video.ToString()))
-                    await bot.SendVideoAsync(msg.Chat.Id, onlineFile);
-
-                return;
-            }
-
-            await bot.SendTextMessageAsync(msg.Chat.Id, "Введено неверное имя файла");
-        }
-
-        public static async Task DeleteFile(Message msg, TelegramBotClient bot, UserDB user)
-        {
-            string[] allFoundFiles = Directory.GetFiles(@$"TelegramBotIQ Users File\{user.GetUser()}\", msg.Text, SearchOption.AllDirectories);
-
-            foreach (var filepath in allFoundFiles)
-            {   
-                System.IO.File.Delete(filepath);
-
-                await bot.SendTextMessageAsync(msg.Chat.Id, "Файл удален!");
-
-                return;
-            }
-
-            await bot.SendTextMessageAsync(msg.Chat.Id, "Введено неверное имя файла");
-        }
 
         public static async Task SendMovies(Message msg, TelegramBotClient bot)
         {
--- TelegramBotIQ/TelegramBotEditor.cs
+++ TelegramBotIQ/TelegramBotEditor.cs
@@ -1,19 +1,10 @@
 ﻿using System;
-using System.Data;
-using System.Text.RegularExpressions;
-
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
-
-using WeatherAPI;
 using ValuteAPI;
-using ConvertPDF;
-
-using System.Threading.Tasks;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.InlineQueryResults;
+using WeatherAPI;
 
 namespace TelegramBotIQ
 {
@@ -23,23 +14,14 @@ namespace TelegramBotIQ
 
         private TelegramBotClient botClient;
 
-        private UserDB userDB;
-
-        private DB db;
-
-        private DataTable table;
-
         public TelegramBotEditor(string telegramBotToken)
         {
             _telegramBotToken = telegramBotToken;
 
             botClient = new TelegramBotClient(_telegramBotToken) { Timeout = TimeSpan.FromSeconds(10) };
-            userDB = new UserDB();
-            db = new DB();
 
-            botClient.OnMessage += Bot_OnMessage;
+            botClient.OnMessage += Bot_OnStart;
             botClient.OnCallbackQuery += Bot_OnCallbackQuery;
-            //botClient.OnInlineQuery += Bot_OnInline;
 
         }
 
@@ -61,8 +43,6 @@ namespace TelegramBotIQ
             botClient.StopReceiving();
         }
 
-        #region Стартовые обработчики
-
         private async void Bot_OnStart(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             var msg = e.Message;
@@ -76,179 +56,15 @@ namespace TelegramBotIQ
 Позже вы сможете попросить наших ботов вам их прислать.
 Для вашей персональной безопасности вам следует зарегистрировать пользователя на нашем сервисе.
 Для этого стоит выбрать кнопку <i>'Регистрация'</i> и ввести логин и пароль.
-После создания аккаунта или если он уже есть, то надо выбрать кнопку <i>'Авторизация'</i>", ParseMode.Html, false, false, 0);
-
-                var inlineFunc = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("Авторизация"),
-                            InlineKeyboardButton.WithCallbackData("Регистрация")
-                        }
-                    }
-                );
-
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Верификация", replyMarkup: inlineFunc);
+После создания аккаунта или если он уже есть, то надо выбрать кнопку.
+Напишите /command", ParseMode.Html, false, false, 0);
 
                 botClient.OnMessage -= Bot_OnStart;
-            }
-        }
-
-        private async void Bot_OnCheck(object sender, CallbackQueryEventArgs e)
-        {
-            var callback = e.CallbackQuery;
-            var text = e.CallbackQuery.Data;
-
-            switch (text)
-            {
-                case "Авторизация":
-
-                    await botClient.SendTextMessageAsync(callback.From.Id, "Введите имя пользователя");
-
-                    botClient.OnMessage += Bot_AutoLogin;
-                    botClient.OnCallbackQuery -= Bot_OnCheck;
-
-                    break;
-
-                case "Регистрация":
-
-                    await botClient.SendTextMessageAsync(callback.From.Id, "Введите имя пользователя");
-
-                    botClient.OnMessage += Bot_RegLogin;
-                    botClient.OnCallbackQuery -= Bot_OnCheck;
-
-                    break;
-            }
-        }
-
-        #endregion
-
-        #region Обработчик регистрации
-
-        private async void Bot_RegLogin(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-            var username = e.Message.Text; 
-
-            Regex.Replace(username, @"[^0-9a-zA-Z]+", "");
-
-            if (username == null)
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Ошибка при регистрации");
-
-                botClient.OnCallbackQuery += Bot_OnCheck;
-                botClient.OnMessage -= Bot_RegLogin;
-            }
-
-            userDB.SetUser(username);
-
-            await botClient.SendTextMessageAsync(msg.From.Id, "Введите пароль");
-
-            botClient.OnMessage += Bot_RegPass;
-            botClient.OnMessage -= Bot_RegLogin;
-        }
-
-        private async void Bot_RegPass(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-            var pass = e.Message.Text;
-
-            if (pass != null)
-            {
-                userDB.SetPass(pass);
-
-                db.RegSQL(userDB);
-
-                await botClient.SendTextMessageAsync(msg.Chat.Id, @$"Вы успешно зарегистрировались! 
-Логин: {userDB.GetUser()},
-Пароль: {userDB.GetPass()}");
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Нажмите /command, чтобы получить список доступных команд");
 
                 botClient.OnMessage += Bot_OnMessage;
-                botClient.OnCallbackQuery += Bot_OnCallbackQuery;
-                botClient.OnCallbackQuery -= Bot_OnCheck;
-                botClient.OnMessage -= Bot_RegPass;
             }
-            else
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Ошибка при регистрации!");
-
-                botClient.OnCallbackQuery += Bot_OnCheck;
-                botClient.OnMessage -= Bot_RegPass;
-            }
-
         }
 
-        #endregion
-
-        #region Обработчик авторизации
-
-        private async void Bot_AutoLogin(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-            var username = e.Message.Text;
-
-            userDB.SetUser(username);
-
-            await botClient.SendTextMessageAsync(msg.From.Id, "Введите пароль");
-
-            botClient.OnMessage += Bot_AutoPass;
-            botClient.OnMessage -= Bot_AutoLogin;
-        }
-
-        private async void Bot_AutoPass(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-            var pass = e.Message.Text;
-
-            userDB.SetPass(pass);
-
-            table = db.AutoSQL(userDB);
-
-            if (table.Rows.Count > 0)
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Вы успешно авторизованы!");
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Нажмите /command, чтобы получить список доступных команд");
-
-                botClient.OnMessage += Bot_OnMessage;
-                botClient.OnCallbackQuery += Bot_OnCallbackQuery;
-                botClient.OnMessage -= Bot_AutoPass;
-                botClient.OnCallbackQuery -= Bot_OnCheck;
-            }
-            else
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Ошибка при авторизации!");
-
-                botClient.OnCallbackQuery += Bot_OnCheck;
-                botClient.OnMessage -= Bot_AutoPass;
-            }
-            
-        }
-
-        #endregion
-
-        //private async void Bot_OnInline(object sender, InlineQueryEventArgs e)
-        //{
-
-        //    InlineQueryResultBase[] result =
-        //    {
-        //        new InlineQueryResultPhoto
-        //        (
-        //            id: "1",
-        //            photoUrl: @"https://photo.sibnet.ru/upload/imgbig/124840052868.jpeg",
-        //            thumbUrl: @"https://photo.sibnet.ru/upload/imgbig/124840052868.jpeg"
-        //            )
-        //        {
-        //            Caption = "Фото"
-        //        },
-
-        //    };
-
-        //    await botClient.AnswerInlineQueryAsync(e.InlineQuery.Id, result, 
-        //                                   isPersonal: true,
-        //                                   cacheTime: 0);
-
-        //}
 
         #region Обработка нажатия кнопок
         private async void Bot_OnCallbackQuery(object sender, CallbackQueryEventArgs e)
@@ -275,76 +91,9 @@ namespace TelegramBotIQ
                     botClient.OnMessage -= Bot_OnMessage;
 
                     break;
-
-                case "PDF to Word":
-
-                    await botClient.AnswerCallbackQueryAsync(callback.Id, "Отправьте файл");
-
-                    botClient.OnMessage += Bot_OnMessageConvert;
-                    botClient.OnMessage -= Bot_OnMessage;
-
-                    break;
-
-                case "Скачать файл":
-
-                    await botClient.AnswerCallbackQueryAsync(callback.Id, "Отправьте название файла");
-
-                    botClient.OnMessage += Bot_DowloadFile;
-                    botClient.OnMessage -= Bot_OnMessage;
-
-                    break;
-
-                case "Удалить файл":
-
-                    await botClient.AnswerCallbackQueryAsync(callback.Id, "Отправьте название файла");
-
-                    botClient.OnMessage += Bot_DeleteFile;
-                    botClient.OnMessage -= Bot_OnMessage;
-
-                    break;
             }
         }
         #endregion
-
-        private async void Bot_DowloadFile(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-
-            try
-            {
-                await BotCommands.SendFile(msg, botClient, userDB);
-            }
-            catch
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Введено неверный формат файла");
-            }
-            finally
-            {
-                botClient.OnMessage += Bot_OnMessage;
-
-                Dispose();
-            }
-        }
-
-        private async void Bot_DeleteFile(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-
-            try
-            {
-                await BotCommands.DeleteFile(msg, botClient, userDB);
-            }
-            catch
-            {
-                await botClient.SendTextMessageAsync(msg.Chat.Id, "Введено неверный формат файла");
-            }
-            finally
-            {
-                botClient.OnMessage += Bot_OnMessage;
-
-                Dispose();
-            }
-        }
 
         #region Обработка входящих сообщений
         private async void  Bot_OnMessage(object sender, MessageEventArgs e)
@@ -355,9 +104,6 @@ namespace TelegramBotIQ
                 && msg.Type != MessageType.Voice && msg.Type != MessageType.Photo && msg.Type != MessageType.Audio)
                     return;
 
-            if (msg.Text == "/Out")
-                await OutUser(msg);
-
                 Console.WriteLine($"{msg.Chat.FirstName} {msg.Chat.LastName} отправил сообщение.");
 
             switch (msg.Type)
@@ -367,11 +113,7 @@ namespace TelegramBotIQ
                     break;
 
                 case MessageType.Text:
-                    await BotCommands.MessageMovie(msg, botClient);
-                    break;
-
-                default:
-                    await BotCommands.MessageReplyFile(msg, botClient, msg.Type, userDB);
+                    await BotCommands.MessageReplyText(msg, botClient);
                     break;
             }
         }
@@ -401,55 +143,8 @@ namespace TelegramBotIQ
         }
         #endregion
 
-        #region Обработчик для конвертора
-        private async void Bot_OnMessageConvert(object sender, MessageEventArgs e)
-        {
-            var msg = e.Message;
-
-            try
-            {
-                await ConvertPDF.Convert.ConvertPDFtoWord(botClient, msg);
-            }
-            catch
-            {
-                Console.WriteLine($"{msg.Chat.FirstName} {msg.Chat.LastName} Проблема с обработкой файла");
-            }
-            finally
-            {
-                botClient.OnMessage += Bot_OnMessage;
-
-                Dispose();
-            }
-        }
-        #endregion
-
-        private async Task OutUser(Message msg)
-        {
-            var inlineFunc = new InlineKeyboardMarkup(new[]
-                    {
-                        new[]
-                        {
-                            InlineKeyboardButton.WithCallbackData("Авторизация"),
-                            InlineKeyboardButton.WithCallbackData("Регистрация")
-                        }
-                    }
-                );
-
-            await botClient.SendTextMessageAsync(msg.Chat.Id, "Верификация", replyMarkup: inlineFunc);
-
-            botClient.OnCallbackQuery += Bot_OnCheck;
-            botClient.OnMessage -= Bot_OnStart;
-            botClient.OnMessage -= Bot_OnMessage;
-            botClient.OnCallbackQuery -= Bot_OnCallbackQuery;
-
-
-        }
-
         public void Dispose()
         {
-            botClient.OnMessage -= Bot_OnMessageConvert;
-            botClient.OnMessage -= Bot_OnMessageWeather;
-            botClient.OnMessage -= Bot_DowloadFile;
         }
     }
 }
